fe45dee85a214f86809e8b89dae5c4ef82b26f84
masterUNG <phrombutr@gmail.com>
2019-04-10 11:01:14 +0700

    test

--- My Tutorial/RegisterViewController.swift
+++ My Tutorial/RegisterViewController.swift
@@ -9,22 +9,31 @@
 import UIKit
 
 class RegisterViewController: UIViewController {
+    
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+    }   // Main Method
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    @IBAction func backButton(_ sender: UIBarButtonItem) {
+        
+        performSegue(withIdentifier: "backHome", sender: nil)
+        
     }
     
 
-    /*
-    // MARK: - Navigation
+    
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
-}
+}   // Main Class
--- My Tutorial/ViewController.swift
+++ My Tutorial/ViewController.swift
@@ -9,12 +9,43 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }   // Main Method
+    
+    @IBAction func loginButton(_ sender: UIButton) {
+        
+        print("You Click LoginButton")
+//        Get Value From TextField
+        let email: String = emailTextField.text!
+        let password: String = passwordTextField.text!
+        print("email ==>> \(email), password ==>> \(password)")
+        
+//        Check Space
+        if (email.count == 0) || (password.count == 0) {
+            print("Have Space")
+        }   else {
+            print("No Space")
+        }
+        
+        
+        
+        
+    }   // loginButton
+    
+    @IBAction func newRegisterButton(_ sender: UIButton) {
+        print("You Click New Register")
+        performSegue(withIdentifier: "GoRegister", sender: nil)
     }
+    
+    
 
 
-}
+}   // Main Class
 
